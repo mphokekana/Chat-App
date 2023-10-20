213214630a13803c057c2e1093fb61787a765bf6
mpho kekana <mphokekana736@gmail.com>
2023-10-20 13:41:37 -0700

    All Changes

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+    return (
+        <Avatar src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg" />
+    )
+}
+
+export default Cat
---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,16 @@
+import React, {Suspense, lazy} from "react";
+
+const Cat = lazy(() => import("../../components/Cat"));
+
+const GeneralApp = () => {
+
+  return (
+    <>
+      <Suspense fallback="Loading...">
+        <Cat />
+      </Suspense>
+    </>
+  );
+};
+
+export default GeneralApp;
